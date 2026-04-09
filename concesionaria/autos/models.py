78bde0a0cc6578345b1bb6151c5fa9db2a74adce
mchena <m.chena@itecriocuarto.org.ar>
2026-04-08 23:26:18 -0300

    modelo de auto corregido

--- concesionaria/autos/models.py
+++ concesionaria/autos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Auto(models):
+class Auto(models.Model):
     marca = models.CharField(max_length=30)
     modelo = models.CharField(max_length=50)
     anio = models.IntegerField()
-    precio = models.DecimalField()
+    precio = models.DecimalField(decimal_places=2, max_digits=10)
     color = models.CharField(max_length=20)
 
     def __str__(self):

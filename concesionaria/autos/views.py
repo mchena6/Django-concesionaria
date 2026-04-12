b449480beadf5656d8b6a0ec0af5cf96fcd413b9
IanChoDK <ian.ibanez@itecriocuarto.org.ar>
2026-04-12 17:12:03 -0300

    urls / y autos/ añadidas

--- concesionaria/autos/views.py
+++ concesionaria/autos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def autos(request):
+    return HttpResponse("hola pagina de autos XD")
+
+def inicio(request):
+    return HttpResponse("hola pagina de inicio XD")

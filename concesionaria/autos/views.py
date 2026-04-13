742657eaa416d8492fedf4cee5ba4c64f1cf4b95
IanChoDK <ian.ibanez@itecriocuarto.org.ar>
2026-04-13 19:05:58 -0300

    edit basico templates de autos, inicio y base con navbar

--- concesionaria/autos/views.py
+++ concesionaria/autos/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def autos(request):
-    return HttpResponse("hola pagina de autos XD")
+    return render(request, "autos/autos.html")
 
 def inicio(request):
-    return HttpResponse("hola pagina de inicio XD")
+    return render(request, "autos/inicio.html")

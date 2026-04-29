b7b9e2a8bffe7894425e9b392663054aeca3bec6
IanChoDK <ian.ibanez@itecriocuarto.org.ar>
2026-04-28 22:40:47 -0300

    model vendedor added

--- concesionaria/autos/models.py
+++ concesionaria/autos/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f"{self.nombre} contacto: {self.email} - {self.telefono}"
+
 class Auto(models.Model):
     marca = models.CharField(max_length=30)
     modelo = models.CharField(max_length=50)
     anio = models.IntegerField()
     precio = models.DecimalField(decimal_places=2, max_digits=10)
     color = models.CharField(max_length=20)
+    vendedor = models.ManyToManyField(Vendedor, default=None, blank=True, null=True, related_name="auto")
 
     def __str__(self):
         return f"{self.marca} {self.modelo} ({self.anio}) - ${self.precio}"
--- concesionaria/autos/views.py
+++ concesionaria/autos/views.py
@@ -3,7 +3,7 @@ from autos.models import Auto
 # Create your views here.
 
 def autos(request):
-    autos = Auto.objects.all()
+    autos = Auto.objects.all().order_by("-id")
     return render(request, "autos/autos.html", {"autos": autos})
 
 def inicio(request):

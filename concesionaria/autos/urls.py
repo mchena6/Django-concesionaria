c8f42f4acaca32cad469c61235d89545505f9d9b
mchena <m.chena@itecriocuarto.org.ar>
2026-05-05 23:06:46 -0300

    rutas agregadas

--- concesionaria/autos/urls.py
+++ concesionaria/autos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns= [
         path("", views.inicio, name="inicio"),
         path("autos/", views.autos, name="autos"),
+        path("crear", views.crear_auto, name="crear_auto"),
+        path("editar/<int:id>", views.editar_auto, name="editar_auto"),
+        path("eliminar/<int:id>",views.eliminar_auto, name="eliminar_auto")
 ]

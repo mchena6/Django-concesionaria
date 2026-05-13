92e5aabe29f49e5eefa28c744eea2b7e06296545
mchena <m.chena@itecriocuarto.org.ar>
2026-05-12 22:49:43 -0300

    datos de vendedor corregido

--- concesionaria/autos/models.py
+++ concesionaria/autos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Vendedor(models.Model):
     nombre = models.CharField(max_length=50)
@@ -8,10 +9,11 @@ class Vendedor(models.Model):
 
     def __str__(self):
         return f"{self.nombre} contacto: {self.email} - {self.telefono}"
-    
+
     class Meta:
         verbose_name = "Vendedor"
         verbose_name_plural = "Vendedores"
+
 
 class Auto(models.Model):
     marca = models.CharField(max_length=30)
@@ -19,7 +21,9 @@ class Auto(models.Model):
     anio = models.IntegerField()
     precio = models.DecimalField(decimal_places=2, max_digits=10)
     color = models.CharField(max_length=20)
-    vendedor = models.ManyToManyField(Vendedor, default=None, blank=True, null=True, related_name="autos")
+    vendedor = models.ManyToManyField(
+        Vendedor, default=None, blank=True, null=True, related_name="autos"
+    )
     activo = models.BooleanField(default=True)
 
     def __str__(self):

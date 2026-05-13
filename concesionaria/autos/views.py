536ac39f54f1602d47ad84a67e8e196e97784e06
mchena <m.chena@itecriocuarto.org.ar>
2026-05-12 22:50:09 -0300

    vista para crear vendedor agregada

--- concesionaria/autos/views.py
+++ concesionaria/autos/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from autos.models import Auto
-from .forms import AutoForm
+from autos.models import Auto, Vendedor
+from .forms import AutoForm, VendedorForm
+
 # Create your views here.
 
+
 def autos(request):
-    autos = Auto.objects.all().order_by("-id")
+    autos = Auto.objects.all().order_by("-id").filter(activo=True)
     return render(request, "autos/autos.html", {"autos": autos})
+
 
 def inicio(request):
     return render(request, "autos/inicio.html")
+
 
 def crear_auto(request):
     if request.method == "POST":
@@ -20,11 +24,12 @@ def crear_auto(request):
         form = AutoForm()
     return render(request, "autos/crear_auto.html", {"form": form})
 
+
 def editar_auto(request, id):
     auto = get_object_or_404(Auto, id=id)
 
     if request.method == "POST":
-        form = AutoForm(request.POST)
+        form = AutoForm(request.POST, instance=auto)
         if form.is_valid():
             form.save()
             return redirect("autos")
@@ -32,6 +37,7 @@ def editar_auto(request, id):
         form = AutoForm(instance=auto)
 
     return render(request, "autos/editar_auto.html", {"form": form})
+
 
 def eliminar_auto(request, id):
     auto = get_object_or_404(Auto, id=id)
@@ -43,3 +49,12 @@ def eliminar_auto(request, id):
     return render(request, "autos/eliminar_auto.html", {"auto": auto})
 
 
+def crear_vendedor(request):
+    if request.method == "POST":
+        form = VendedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("autos")
+    else:
+        form = VendedorForm()
+        return render(request, "autos/crear_vendedor.html", {"form": form})

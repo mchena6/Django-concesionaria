37e7f126030ec1a257b223f78918030a9d2d410e
mchena <m.chena@itecriocuarto.org.ar>
2026-05-12 22:49:56 -0300

    ruta para crear vendedor agregada

--- concesionaria/autos/urls.py
+++ concesionaria/autos/urls.py
@@ -6,5 +6,6 @@ urlpatterns= [
         path("autos/", views.autos, name="autos"),
         path("crear", views.crear_auto, name="crear_auto"),
         path("editar/<int:id>", views.editar_auto, name="editar_auto"),
-        path("eliminar/<int:id>",views.eliminar_auto, name="eliminar_auto")
+        path("eliminar/<int:id>",views.eliminar_auto, name="eliminar_auto"),
+        path("crear_vendedor", views.crear_vendedor, name="crear_vendedor"),
 ]

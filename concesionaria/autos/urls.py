340691d7126eecbb61259ef399607ad842959600
IanChoDK <ian.ibanez@itecriocuarto.org.ar>
2026-05-26 22:13:27 -0300

    login y registrarse

--- concesionaria/autos/urls.py
+++ concesionaria/autos/urls.py
@@ -8,4 +8,5 @@ urlpatterns= [
         path("editar/<int:id>", views.editar_auto, name="editar_auto"),
         path("eliminar/<int:id>",views.eliminar_auto, name="eliminar_auto"),
         path("crear_vendedor", views.crear_vendedor, name="crear_vendedor"),
+        path("registrarse/", views.registrarse, name="registrarse"),
 ]
--- concesionaria/concesionaria/settings.py
+++ concesionaria/concesionaria/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "concesionaria.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,6 +67,10 @@ TEMPLATES = [
         },
     },
 ]
+
+
+LOGIN_REDIRECT_URL = 'autos'
+LOGOUT_REDIRECT_URL = 'inicio'
 
 WSGI_APPLICATION = "concesionaria.wsgi.application"
 
